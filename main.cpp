a8fcdb87dd14a4d8c037642183d27659684198af
DHANUKA <danuka1999lakshan@gmail.com>
2022-05-20 23:08:30 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class customer{
+private:
+  int custId;
+  string custName;
+  string custAdress;
+  string custGender;
+  string custEmail;
+
+public:
+  void setDetails
+};
+int main() {
+  
+  return 0;
+
+}
